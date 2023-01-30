1ef8ad81f969d53cda80e8ac57c3030daed547da
ogremanji@gmail.com <ogremanji@gmail.com>
2023-01-29 19:26:21 -0800

    added and completed project_4

---
+++ project_4/script.js
@@ -0,0 +1,39 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
+function caculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    fetch(`https://v6.exchangerate-api.com/v6/9fa71248baea00a78b3164d7/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data);
+        const rate = data.conversion_rates[currency_two];
+
+        rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+        amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', caculate);
+amountEl_one.addEventListener('input', caculate);
+currencyEl_two.addEventListener('change', caculate);
+amountEl_two.addEventListener('input', caculate);
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    caculate();
+})
+
+caculate();
